5aa3b6fb23fef215831cbc567d1da9ebd713067d
Sleyter Monteiro <sleyter.monteiro@colombbus.org>
2022-05-02 16:56:56 +0200

    rappelJS

---
+++ RappelJSModerne/script.js
@@ -0,0 +1,46 @@
+// Scope
+// alert('test liaison');
+
+// function foo() {
+
+//     if(true){
+
+//         var x =5;
+
+//     }
+
+//     console.log(x);
+// }
+
+// Redeclaration 
+
+// var x = 5;
+
+// var x = 10;
+// console.log(x);
+
+
+// Hoisting
+
+// foo();
+// function foo() {
+
+//     console.log('hello');
+
+// }
+
+// let x = 10;
+// console.log(x);
+
+// Global 
+
+// var maVarX = 10;
+// console.log(window);
+
+// Différence basique
+
+const x = {a: 5};
+x.a = 10;
+
+console.log(x);
+
